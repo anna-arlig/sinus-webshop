048044a8f41171d6b5f561e75111ddf45dc7b20e
Markus <markus.mork@iths.se>
2022-02-18 10:35:06 +0100

    Adds mock API and test action and mutation This is to test requesting data from api and saving it in store.

---
+++ src/api/mock.js
@@ -0,0 +1,6 @@
+export async function fetchItems(){
+    return [
+      {id:1, content: 'foo', title: 'lol'},
+      {id:2, content: 'bar', title: 'zomg'},
+    ]
+  }
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as API from '@/api/mock.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    itemList: [],
+    items: {}
   },
   mutations: {
+    savePosts(state, items){
+      for(let item of items){
+        state.postList.push(item)   
+        Vue.set(state.items, item.id, item)   
+      }
+    }
   },
   actions: {
+    async fetchItems(context){
+      const response = await API.fetchItems()
+      context.commit(Mutations.SAVE_POSTS, response)
+    }
   },
   modules: {
   }
