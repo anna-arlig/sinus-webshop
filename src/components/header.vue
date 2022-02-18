901275c95c07a255379aef60dd95a1c74a304e0e
Markus <markus.mork@iths.se>
2022-02-18 11:42:07 +0100

    unimportant

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="header">
     <div id="nav">
-      <img src="../assets/sinus-logo.svg" alt="" height="50px" width="50px">
+      <img src="../assets/images/sinus-logo.svg" alt="" height="50px" width="50px">
       <a>Skateboards</a>
       <a>Apparel</a>
       <a>Accessories</a>
       <a>Brands</a>
 
       <img src="../assets/user-icon.svg" alt="">
-      <img src="" alt="">
+      <img src="../assets/" alt="">
       <img src="" alt="">
 
     </div>
