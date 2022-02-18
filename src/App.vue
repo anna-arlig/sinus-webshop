2665e7bb085b17160859250ee2b96361a0763113
Niklas Andersson <niklas.andersson@iths.se>
2022-02-18 14:21:04 +0100

    Begin home-view add hero background

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,15 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Header from './components/header.vue'
 
 export default {
   components: {Header}
 }
 </script>
-<style lang="scss" scoped>
+
+<style lang="scss">
 @import './assets/styles/fonts-colors.scss';
 @import './assets/styles/mixins.scss';
 *{
@@ -31,6 +32,7 @@ body, html{
     max-width: 1440px;
     h1, h2, h3, h4, h5{
       font-family: $heading;
+      margin: 0;
     }
     p, button{
       font-family: $paragraph;
