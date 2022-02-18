cb3bd5cdc5b702cc71a5c1e0d2a40cee8a55cc4f
anna.arlig <you@example.com>
2022-02-18 10:31:42 +0100

    Add multiple components

---
+++ src/components/cartPopup.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="cartPopup"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/menuDropdown.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="menuDropdown"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
