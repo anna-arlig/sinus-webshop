9771d86d98e3e235fc169a21c4e1d28101db0160
Niklas Andersson <niklas.andersson@iths.se>
2022-02-18 11:49:16 +0100

    Add base styling i #app

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,29 @@
     <router-view/>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import './assets/styles/fonts-colors.scss';
+*{
+  box-sizing: border-box;
+}
+body, html{
+  margin: 0;
+  padding: 0;  
+  font-size: 16px;
+  line-height: 1.6;
+  list-style: none;
+  text-decoration: none;
+  @include flex-col-center;
+  #app{
+    max-width: 1440px;
+    h1, h2, h3, h4, h5{
+      font-family: $heading;
+    }
+    p, button{
+      font-family: $paragraph;
+    }
+
+  }
+}
+</style>
