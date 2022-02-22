5b00acccf291cce7e8c01a8349e7fe42334ef233
Markus <markus.mork@iths.se>
2022-02-22 14:03:25 +0100

    commit to pull

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
   state: {
     productList: [], 
     products: {}, 
-    logInPopup: false
+    logInPopup: false,
     user: '',
     searchResults: [],
     searchTerms: [...SearchTerms],
