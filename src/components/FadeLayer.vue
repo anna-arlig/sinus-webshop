f57bf671739cff0c60584b8fa304e142c87ccbb9
Linda Marko <lindamarko83@gmail.com>
2022-02-27 08:14:57 +0100

    Fix fadelayer and carousel

--- src/components/FadeLayer.vue
+++ src/components/FadeLayer.vue
@@ -4,22 +4,22 @@
 
 <script>
 export default {
-  computed:{
-    fading(){
-      return this.$store.state.modal
-    }
-  }
+  computed: {
+    fading() {
+      return this.$store.state.showLogIn
+    },
+  },
 }
 </script>
 
 <style>
-.fade{
-      background-color: #000000;
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      right: 0;
-      left: 0;
-      opacity: .6;
-    }
+.fade {
+  background-color: #000000;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  opacity: 0.6;
+}
 </style>
